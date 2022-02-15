772e76bc5228d913c6ce5aebbe2d0428673741ac
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-14 19:41:29 -0500

    Add mobile design for search bar

--- src/components/main/searchbar/SearchBar.js
+++ src/components/main/searchbar/SearchBar.js
@@ -1,6 +1,34 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
+import searchIcon from "../../../assets/desktop/icon-search.svg";
+import filterIcon from "../../../assets/mobile/icon-filter.svg";
 
 export default function SearchBar(props) {
-  return <form className={styles["search__form"]}></form>;
+  return (
+    <form className={styles["search__form"]}>
+      <input
+        type="text"
+        placeholder="Filter by title..."
+        className={styles["search__title"]}
+      />
+
+      <div className={styles.other}>
+        <input type="text" placeholder="Filter by location..." />
+
+        <label for="fulltime">
+          <input type="checkbox" id="fulltime" />
+          Full Time Only
+        </label>
+        <button type="submit">Search</button>
+      </div>
+
+      <butto className={styles["search__filter_btn"]}>
+        <img src={filterIcon} className={styles["search__filter_btn_icon"]} />
+      </butto>
+
+      <button type="submit" className={styles["search__mobile_btn"]}>
+        <img src={searchIcon} className={styles["search__mobile_btn_icon"]} />
+      </button>
+    </form>
+  );
 }
