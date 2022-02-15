f7a357640772b043b0b8766f4ab22617fb91edb8
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-15 11:41:48 -0500

    Add mobile location input styling

--- src/components/main/searchbar/SearchBar.js
+++ src/components/main/searchbar/SearchBar.js
@@ -1,25 +1,33 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
 import searchIcon from "../../../assets/desktop/icon-search.svg";
+import locationIcon from "../../../assets/desktop/icon-location.svg";
 import filterIcon from "../../../assets/mobile/icon-filter.svg";
 
 export default function SearchBar(props) {
   return (
     <form className={styles["search__form"]}>
-      <input
-        type="text"
-        placeholder="Filter by title..."
-        className={styles["search__title"]}
-      />
+      <div className={styles["serach__title_container"]}>
+        <input
+          type="text"
+          placeholder="Filter by title..."
+          className={styles["search__title"]}
+        />
+      </div>
 
       <div className={styles.other} aria-modal="true" aria-labelledby="filter">
-        <input type="text" placeholder="Filter by location..." />
+        <div className={styles["search__location_container"]}>
+          <img src={locationIcon} />
+          <input type="text" placeholder="Filter by location..." />
+        </div>
 
-        <label for="fulltime">
+        <label for="fulltime" className={styles["search__fulltime"]}>
           <input type="checkbox" id="fulltime" />
           Full Time Only
         </label>
-        <button type="submit">Search</button>
+        <button type="submit" className={styles["search__btn"]}>
+          Search
+        </button>
       </div>
 
       <button
