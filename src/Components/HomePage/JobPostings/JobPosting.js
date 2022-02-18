bf4eb4f3f8d086c8dc67849b27b9accdf290a77c
Thomas Kressman <thomas.kressman@gmail.com>
2022-02-18 14:13:17 -0500

    Update aria roles

--- src/Components/HomePage/JobPostings/JobPosting.js
+++ src/Components/HomePage/JobPostings/JobPosting.js
@@ -2,11 +2,7 @@ import styles from "./JobPosting.module.css";
 import { Link } from "react-router-dom";
 export default function JobPosting({ job }) {
   return (
-    <Link
-      to={`/job/${job.id}`}
-      style={{ textDecoration: "none" }}
-      role="listitem"
-    >
+    <Link to={`/job/${job.id}`} style={{ textDecoration: "none" }}>
       <article className={styles["job__posting"]}>
         <figure
           className={styles["job__logo_container"]}
